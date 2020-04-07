2d75b95fbdbd198d39facaa0e35fcd7fd7ed39f2
Herokuabsyz <53526113+Herokuabsyz@users.noreply.github.com>
2020-04-07 17:27:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -38,9 +38,9 @@ $i = 0;
 //$myEndpointSuffix = getenv('EndpointSuffix');
 foreach ($response->records as $record)
 {
-echo $record->fields->LastName;
-echo $i;
-echo $record->fields->ContentDocumentIds__c."<br>";
+//echo $record->fields->LastName;
+//echo $i;
+//echo $record->fields->ContentDocumentIds__c."<br>";
 $response = $mySforceConnection->retrieve('Id, Title,VersionData','ContentVersion', explode (",", $record->fields->ContentDocumentIds__c));
 $zip_name = $record->fields->LastName.'.zip';
 $zip = new ZipArchive();
@@ -58,48 +58,8 @@ if ($zip->open($zip_name, ZipArchive::CREATE) != TRUE )
 }
 $zip->close();
 }
-	echo 'after zip';
 //$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('AccountName').";AccountKey=".getenv('AccountKey').";EndpointSuffix=".getenv('EndpointSuffix');
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('AccountName').";AccountKey=".getenv('AccountKey');
-//$connectionString = "DefaultEndpointsProtocol=https;AccountName=azureabsyz;AccountKey=1nqwRoip8tEOkLZSk3KoSj2NoazUXM2YrQJstNQE6w7bRQJkiVt1X5MsZzWyAuzqsUziC5vuN0eWWDEd4Mj5aw==;EndpointSuffix=core.windows.net";
-//DefaultEndpointsProtocol=https;AccountName=archivefilesstorage;AccountKey=kc2KM1p0AxCnKTQ3QNfrYe4mTNzQAqhViNB7O/ObXWFKY2y8HJvFzS0xbVL+BfE3Xi6o+DIQptQJKwgHtlVT7A==;EndpointSuffix=core.windows.net
-// Create blob client.
-	echo 'afer connect';
-	# Setup a specific instance of an Azure::Storage::Client
-    //$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('account_name').";AccountKey=".getenv('account_key');
-    
-    // Create blob client.
-   /* $blobClient = BlobRestProxy::createBlobService($connectionString);
-    
-    # Create the BlobService that represents the Blob service for the storage account
-    $createContainerOptions = new CreateContainerOptions();
-    
-    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-    
-    // Set container metadata.
-    $createContainerOptions->addMetaData("key1", "value1");
-    $createContainerOptions->addMetaData("key2", "value2");
-
-    $containerName = "blockblobs".generateRandomString();
-
-    try    {
-        // Create container.
-	$blobClient->createContainer($containerName, $createContainerOptions);
-	
-	$myfile = fopen("HelloWorld.txt", "w") or die("Unable to open file!");
-    fclose($myfile);
-
-    # Upload file as a block blob
-    echo "Uploading BlockBlob: ".PHP_EOL;
-    echo $fileToUpload;
-    echo "<br />";
-    
-    $content = fopen($fileToUpload, "r");
-
-    //Upload blob
-    $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-	}*/
-		
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 echo 'blobclient';
 $fileToUpload = $zip_name;
@@ -112,10 +72,9 @@ echo 'filetoupload';
     // Set container metadata.
     $createContainerOptions->addMetaData("key1", "value1");
     $createContainerOptions->addMetaData("key2", "value2");
-echo 'container';
-      $containerName = "blockblobs".generateRandomString();
-	 echo $containerName;
-	//$containerName = $zip_name;
+	
+    $containerName = "blockblobs".generateRandomString();
+	
     try {
         // Create container.
         $blobClient->createContainer($containerName, $createContainerOptions);
@@ -126,7 +85,7 @@ echo 'container';
         
         $content = fopen($fileToUpload, "r");
         $blobClient->createBlockBlob($containerName,$fileToUpload,$content);
-		$listBlobsOptions = new ListBlobsOptions();
+	$listBlobsOptions = new ListBlobsOptions();
         $listBlobsOptions->setPrefix("HelloWorld");
         echo "These are the blobs present in the container: ".$containerName."<br>";
         
@@ -147,12 +106,7 @@ echo 'container';
         $error_message = $e->getMessage();
         echo $code.": ".$error_message."<br />";
     }
-	$i=$i+1;
-}
-$response2 = $mySforceConnection->create($records);
- 
+} 
 ?>
-
-
 </body>
 </html>
